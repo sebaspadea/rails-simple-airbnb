14207b32cd4b816bd8d34af57c60adafeda2d5fe
Sebastian Spadea <spadea.sebastian@gmail.com>
2020-07-22 17:07:03 -0300

    Simple AirBnB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creando flats perra"
+  Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Rome',
+  address: '10 Clifton Gardens Rome W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Madrid',
+  address: '10 Clifton Gardens Madrid W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 125,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Berlin',
+  address: '10 Clifton Gardens Berlin W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Three double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 150,
+  number_of_guests: 6
+)
+puts "Flats creados bitchi!"
